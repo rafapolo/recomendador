27bcf814a636569bcdb8bd8765521b94906ba115
Polo <rafael.polo@gmail.com>
2012-03-19 16:06:49 -0300

    1

--- app.rb
+++ app.rb
@@ -59,7 +59,7 @@ get "/" do
     @likes   = @graph.get_connections('me', 'likes')
 
     # for other data you can always run fql
-    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square, work FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
+    @friends_using_app = @graph.fql_query("SELECT uid, name, is_app_user, pic_square FROM user WHERE uid in (SELECT uid2 FROM friend WHERE uid1 = me()) AND is_app_user = 1")
   end
   erb :index
 end
